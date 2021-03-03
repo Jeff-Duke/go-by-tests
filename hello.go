ddcaa12a3133038e95effc852e24a1474678d60c
Jeff Duke <jeff.duke@gmail.com>
2021-03-02 17:09:31 -0700

    add Spanish language support

--- hello.go
+++ hello.go
@@ -5,14 +5,18 @@ import "fmt"
 const englishGreeting = "Hello, "
 
 // Hello prints a hello world message
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
+	}
+
+	if language == "Spanish" {
+		return "Hola, " + name
 	}
 
 	return englishGreeting + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -12,14 +12,20 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("says hello to people", func(t *testing.T) {
-		got := Hello("Jeff")
+		got := Hello("Jeff", "")
 		want := "Hello, Jeff"
 		assertCorrectGreeting(t, got, want)
 	})
 
 	t.Run("says hello to the world if no name given", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world"
+		assertCorrectGreeting(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
 		assertCorrectGreeting(t, got, want)
 	})
 }
